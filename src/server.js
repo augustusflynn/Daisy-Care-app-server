c201765bb6ae68bd6c8be5dd936491eae10bfb53
Augustus Flynn <huytung139@gmail.com>
2021-10-24 14:32:02 +0700

    update

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ import bodyParser from 'body-parser'
 import viewEngine from './config/viewEngine'
 import initWebRoutes from './route/web'
 import connectDB from './config/connectDb'
-
+import cors from 'cors'
 require('dotenv').config();
 
 let app = express();
-app.use(cors({}));
+app.use(cors());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
