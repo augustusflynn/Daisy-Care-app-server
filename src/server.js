80141411c0913cd26b4549835a30b0c39354e090
Augustus Flynn <huytung139@gmail.com>
2021-12-09 23:15:12 +0700

    update

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ let app = express();
 app.use(function (req, res, next) {
   const allowedOrigins = [process.env.URL_REACT1, process.env.URL_REACT2, process.env.URL_REACT3];
   const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
+  if (origin && allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
   // Website you wish to allow to connect
